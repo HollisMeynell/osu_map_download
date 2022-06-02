788489381f8b99eff320114c5218457b7a1df342
Avimitin <avimitin@gmail.com>
2022-06-02 09:41:52 +0800

    Code cleanup
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/bin/download_cli.rs
+++ src/bin/download_cli.rs
@@ -1,12 +1,10 @@
 use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 use directories::BaseDirs;
-use osu_map_download::util::{do_home, do_login, download, OsuMapDownloadError, UserSession};
+use osu_map_download::util::{do_home, do_login, download, UserSession};
 use serde::{Deserialize, Serialize};
-use std::{env, fs};
-use std::io::Write;
+use std::fs;
 use std::path::{Path, PathBuf};
-use reqwest::StatusCode;
 
 #[derive(Debug, Parser)]
 #[clap(name = "osu beatmap downloader")]
@@ -31,17 +29,10 @@ struct Config {
     session: String,
 }
 
-async fn run(sid:u64, user:&mut UserSession) -> Result<()> {
-
-
+async fn run(sid: u64, user: &mut UserSession) -> Result<()> {
     println!("正在下载...");
 
-    download(
-        sid,
-        user,
-        Path::new(&format!(r"{}.zip", sid)),
-    )
-    .await?;
+    download(sid, user, Path::new(&format!(r"{}.zip", sid))).await?;
 
     println!("下载完成");
     Ok(())
@@ -63,18 +54,21 @@ fn get_config_path() -> Result<PathBuf> {
     if !file.is_file() {
         let file = fs::File::create(file);
         if file.is_err() {
-           anyhow!("创建配置文件失败,请检查程序目录下是否有名为'config.json'的文件夹");
+            return Err(anyhow!(
+                "创建配置文件失败,请检查程序目录下是否有名为'config.json'的文件夹"
+            ));
         }
     }
-    return Ok(dir)
+
+    Ok(dir)
 }
 
-fn read_config(path:&Path) -> Result<UserSession>{
-
+fn read_config(path: &Path) -> Result<UserSession> {
     let config = fs::read(path).with_context(|| "读取用户配置失败")?;
-    let config: Config = serde_json::from_slice(&config)
-        .with_context(|| "解析用户配置失败,请使用'-l'参数登录,或者请加'-c'参数重置配置后重新运行")?;
-    let mut user = UserSession::new(&config.username,&config.password);
+    let config: Config = serde_json::from_slice(&config).with_context(|| {
+        "解析用户配置失败,请使用'-l'参数登录,或者请加'-c'参数重置配置后重新运行"
+    })?;
+    let mut user = UserSession::new(&config.username, &config.password);
     user.read_session(&config.session);
 
     Ok(user)
@@ -82,15 +76,15 @@ fn read_config(path:&Path) -> Result<UserSession>{
 
 fn save_user_cookie(user: &mut UserSession, user_config_path: &Path) -> Result<()> {
     let config = fs::read(user_config_path).with_context(|| "读取用户配置失败")?;
-    let mut  config: Config = serde_json::from_slice(&config)
-        .with_context(|| "解析用户配置失败,请使用'-l'参数登录,或者请加'-c'参数重置配置后重新运行")?;
+    let mut config: Config = serde_json::from_slice(&config).with_context(|| {
+        "解析用户配置失败,请使用'-l'参数登录,或者请加'-c'参数重置配置后重新运行"
+    })?;
     config.session = user.save_session();
-    let config_str =
-        serde_json::to_string(&config)?;
+    let config_str = serde_json::to_string(&config)?;
     fs::write(user_config_path, config_str.as_bytes())?;
     Ok(())
 }
-async fn login_no_name() -> Result<()>{
+async fn login_no_name() -> Result<()> {
     println!("enter osu name:");
     let mut username = String::new();
     std::io::stdin().read_line(&mut username)?;
@@ -102,19 +96,18 @@ async fn login_no_name() -> Result<()>{
     do_login(&mut user).await?;
     println!("success!");
 
-    let config = Config{
+    let config = Config {
         username,
         password,
-        session:user.save_session(),
+        session: user.save_session(),
     };
-    let config_str =
-        serde_json::to_string(&config)?;
+    let config_str = serde_json::to_string(&config)?;
     let file = get_config_path()?;
     fs::write(file.as_path(), config_str.as_bytes())?;
 
     Ok(())
 }
-async fn login_name(username: &String) -> Result<()>{
+async fn login_name(username: &String) -> Result<()> {
     let password = rpassword::prompt_password(format!("enter {username}'s password:\n"))?;
     println!("login. . .");
     let mut user = UserSession::new(username, &password);
@@ -122,39 +115,47 @@ async fn login_name(username: &String) -> Result<()>{
     do_login(&mut user).await?;
     println!("success!");
 
-    let config = Config{
+    let config = Config {
         username: username.clone(),
         password,
-        session:user.save_session(),
+        session: user.save_session(),
     };
-    let config_str =
-        serde_json::to_string(&config)?;
+    let config_str = serde_json::to_string(&config)?;
     let file = get_config_path()?;
     fs::write(file.as_path(), config_str.as_bytes())?;
 
     Ok(())
 }
 
+async fn handle_login(cli: &Cli) -> Result<()> {
+    if let Some(s) = &cli.user {
+        login_name(s).await?;
+    } else {
+        login_no_name().await?;
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let cli:Cli = Cli::parse();
+    let cli: Cli = Cli::parse();
     if cli.clear {
         let path = get_config_path()?;
-        fs::remove_file(path.as_path());
+        fs::remove_file(path.as_path())?;
     }
+
     if cli.login {
-       if let Some(s) = cli.user {
-           login_name(&s).await?;
-       } else {
-           login_no_name().await?;
-       }
-    } else {
-        if let Some(sid) = cli.sid {
-            let path = get_config_path()?;
-            let mut user = read_config(path.as_path())?;
-            run(sid, &mut user).await?;
-            save_user_cookie(&mut user, path.as_path());
-        }
+        handle_login(&cli).await?;
+        return Ok(());
     }
+
+    if let Some(sid) = cli.sid {
+        let path = get_config_path()?;
+        let mut user = read_config(path.as_path())?;
+        run(sid, &mut user).await?;
+        save_user_cookie(&mut user, path.as_path())?;
+    }
+
     Ok(())
 }
